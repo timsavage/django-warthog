5943d8661b0d0449d1de5fd090654dd96bc6da7b
Tim Savage <tim.savage@poweredbypenguins.org>
2016-02-15 01:09:41 +1100

    Remove use of urls.patterns

--- warthog/urls.py
+++ warthog/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from django.conf.urls import *
+from django.conf.urls import url
 from .views import CmsPreview
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^_preview/(\d+)/$', CmsPreview.as_view(), name='warthog-preview'),
-)
+]
