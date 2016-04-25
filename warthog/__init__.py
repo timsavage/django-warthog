ae1ba8e0d3a6aaaad36111b79db923a1f0b587b4
Tim Savage <tim.savage@poweredbypenguins.org>
2016-04-25 16:37:44 +1000

    Moved AppConfig into app_config.py. Solves issue when installing before Django is installed crashes installer.

--- warthog/__init__.py
+++ warthog/__init__.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.apps import AppConfig
-
-
-class WarthogAppConfig(AppConfig):
-    name = 'warthog'
-    verbose_name = 'Warthog CMS'
-
-
-default_app_config = 'warthog.WarthogAppConfig'
-
 VERSION = (0, 4, 2, 'beta')
 __version__ = '.'.join(map(str, VERSION))
+
+default_app_config = 'warthog.app_config.WarthogAppConfig'
---
+++ warthog/app_config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class WarthogAppConfig(AppConfig):
+    name = 'warthog'
+    verbose_name = 'Warthog CMS'
