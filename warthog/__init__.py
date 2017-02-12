553d50d293cb07f477364d4bc7577caa8652b4d4
Tim Savage <tim@savage.company>
2017-02-12 17:53:14 +1100

    Bumped version

--- warthog/__init__.py
+++ warthog/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (0, 4, 2, 'beta')
+VERSION = (0, 5, 0, 'beta')
 __version__ = '.'.join(map(str, VERSION))
 
 default_app_config = 'warthog.app_config.WarthogAppConfig'
